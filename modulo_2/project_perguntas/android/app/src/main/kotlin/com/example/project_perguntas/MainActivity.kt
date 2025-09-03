443ea2ecbcafa5436063cbd483e394f66ce6a9e7
jsantini <jean.ferreira.santini@gmail.com>
2025-09-03 13:41:16 -0300

    Modulo 2

---
+++ modulo_2/project_perguntas/android/app/src/main/kotlin/com/example/project_perguntas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_perguntas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ modulo_2/project_perguntas/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Perguntas')),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada!');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3!!!'),
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
