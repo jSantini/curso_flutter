7e3ac5df07d59b8ec2333d07763c3280705eda3b
jsantini <jean.ferreira.santini@gmail.com>
2025-09-04 20:40:33 -0300

    building chart view

---
+++ modulo_3/expenses/lib/components/chart.dart
@@ -0,0 +1,44 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentsTransactions;
+
+  const Chart(this.recentsTransactions, {super.key});
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentsTransactions.length; i++) {
+        bool sameDay = recentsTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentsTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentsTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentsTransactions[i].value;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay)[0]);
+      print(totalSum);
+
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    groupedTransactions;
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: [
+        
+      ],
+    ),
+    );
+  }
+}
--- modulo_3/expenses/lib/main.dart
+++ modulo_3/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/models/transaction.dart';
@@ -48,19 +49,31 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'Novo Tênis de Corrida',
-    //   value: 310.76,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Conta de Luz',
-    //   value: 211.30,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: 't0',
+      title: 'Conta Antiga',
+      value: 400.00,
+      date: DateTime.now().subtract(Duration(days: 33)),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tr) {
+      return tr.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
 
   _addTransaction(String title, double value) {
     final newTransaction = Transaction(
@@ -106,13 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Card(
-                elevation: 5,
-                color: Colors.blue,
-                child: Text('Gráfico'),
-              ),
-            ),
+            Chart(_recentTransactions),
             Column(children: [TransactionList(_transactions)]),
           ],
         ),
