af45c17639500621557043e3ed19cf674c9083a9
jsantini <jean.ferreira.santini@gmail.com>
2025-09-04 13:07:38 -0300

    formate date

--- modulo_3/expenses/lib/main.dart
+++ modulo_3/expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -79,7 +80,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
