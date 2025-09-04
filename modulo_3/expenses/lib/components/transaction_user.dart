8e93f6130bce3fb471975760a6f77a41c73467ea
jsantini <jean.ferreira.santini@gmail.com>
2025-09-04 14:20:44 -0300

    comunication between widgets

--- modulo_3/expenses/lib/components/transaction_user.dart
+++ modulo_3/expenses/lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
@@ -6,14 +8,11 @@ import 'package:expenses/models/transaction.dart';
 class TransactionUser extends StatefulWidget {
   const TransactionUser({super.key});
 
-  
-
   @override
   State<TransactionUser> createState() => _TransactionUserState();
 }
 
 class _TransactionUserState extends State<TransactionUser> {
-
   final _transactions = [
     Transaction(
       id: 't1',
@@ -29,13 +28,25 @@ class _TransactionUserState extends State<TransactionUser> {
     ),
   ];
 
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TransactionList(_transactions),
-        TransactionForm()
+        TransactionForm(_addTransaction),
       ],
-    )
+    );
   }
 }
